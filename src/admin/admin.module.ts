601fb2db8370ef6166b4678ec82ed69b478c3ea7
Abdelrahman Maysara Mousa <amaysara@aaib.com>
2022-08-11 17:27:02 +0200

    feat: removed unused admin contorller

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AdminController } from './admin.controller';
 import { Admin } from './admin.entity';
 import { AdminService } from './admin.service';
 import { AdminRepository } from './admin.repository';
 @Module({
   imports: [TypeOrmModule.forFeature([Admin])],
   providers: [AdminService, AdminRepository],
-  controllers: [AdminController],
   exports: [AdminService],
 })
 export class AdminModule {}
--- src/shared/auth/auth.controller.ts
+++ src/shared/auth/auth.controller.ts
@@ -2,7 +2,9 @@ import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local.guard';
 import { LoginInput } from '../../admin/inputs/login.dto';
+import { ApiTags } from '@nestjs/swagger';
 @Controller()
+@ApiTags('Auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
