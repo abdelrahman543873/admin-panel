162262d2da7b764822889e8856a8c82daef0114c
abdelrahman mousa <abdelrahmanmysara33@gmail.com>
2022-08-20 14:51:24 +0200

    redner frontend`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
+import { AdminModule } from './admin/admin.module';
 import { AppService } from './app.service';
 import { MerchantModule } from './merchant/merchant.module';
 import { AuthModule } from './shared/auth/auth.module';
-import { AdminModule } from './admin/admin.module';
 import { AppConfigModule } from './shared/config/app.config.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [AppConfigModule, AuthModule, AdminModule, MerchantModule],
   providers: [AppService],
+  controllers: [AppController],
 })
 export class AppModule {}
