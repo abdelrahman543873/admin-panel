f49e26cf62a3a7adba26b413711850eed3f4c6b5
Abdelrahman Maysara Mousa <amaysara@aaib.com>
2022-08-23 12:28:44 +0200

    feat: refactored login imports

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,13 +2,13 @@ import { Controller, Get, Render } from '@nestjs/common';
 
 @Controller()
 export class AppController {
+  @Get('login')
+  @Render('pages/login')
+  renderLogin() {}
+
   @Get('home')
   @Render('index')
   renderHome() {}
-
-  @Get()
-  @Render('pages/login')
-  renderLogin() {}
 
   @Get('merchant-addition')
   @Render('layouts/merchant/merchant-addition')
