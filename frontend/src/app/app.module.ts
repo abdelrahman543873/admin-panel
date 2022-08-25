99f9fdafb44be0d9e96197837a226f177ef06eac
Abdelrahman Maysara Mousa <amaysara@aaib.com>
2022-08-25 12:38:58 +0200

    feat: added routing module to app

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,19 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
-import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
-import { NavbarComponent } from './shared/components/navbar/navbar.component';
-import { PreloaderComponent } from './shared/components/preloader/preloader.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FooterComponent,
-    SidebarComponent,
-    NavbarComponent,
-    PreloaderComponent,
-  ],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ frontend/src/app/home/home.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from '../shared/components/footer/footer.component';
+import { SidebarComponent } from '../shared/components/sidebar/sidebar.component';
+import { NavbarComponent } from '../shared/components/navbar/navbar.component';
+import { PreloaderComponent } from '../shared/components/preloader/preloader.component';
+import { HomeRoutingModule } from './home-routing.module';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    FooterComponent,
+    SidebarComponent,
+    NavbarComponent,
+    PreloaderComponent,
+  ],
+  imports: [CommonModule, HomeRoutingModule],
+})
+export class HomeModule {}
