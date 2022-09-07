d027b157571ea7bb776d6fa0bc7dfd5dac0a26a8
abdelrahman mousa <abdelrahmanmysara33@gmail.com>
2022-09-07 19:24:01 +0200

    feat: added dockerization for backend:wq

--- backend/src/shared/config/app.config.module.ts
+++ backend/src/shared/config/app.config.module.ts
@@ -10,8 +10,8 @@ import { join } from 'path';
     }),
     TypeOrmModule.forRoot({
       type: process.env.DB_TYPE as any,
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
+      host: process.env.DB_HOST_IN_DOCKER || process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT_IN_DOCKER || process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
