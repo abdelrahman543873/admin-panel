f305629069a5535152536f9eba7e5f8d0c8a8f03
abdelrahman mousa <abdelrahmanmysara33@gmail.com>
2022-09-10 08:07:15 +0200

    feat: updated task def and added db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,10 +6,7 @@ import { AuthModule } from './shared/auth/auth.module';
 import { AppConfigModule } from './shared/config/app.config.module';
 
 @Module({
-  imports: [
-    AppConfigModule,
-    //  AuthModule, AdminModule, MerchantModule
-  ],
+  imports: [AppConfigModule, AuthModule, AdminModule, MerchantModule],
   providers: [AppService],
 })
 export class AppModule {}
