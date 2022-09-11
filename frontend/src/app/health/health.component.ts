927f66ac290d9b2b1b46fc6c33f15ecaff62b1b4
abdelrahman mousa <abdelrahmanmysara33@gmail.com>
2022-09-11 16:54:52 +0200

    feat: added route returing in angular

--- frontend/src/app/health/health.component.ts
+++ frontend/src/app/health/health.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-health',
   templateUrl: './health.component.html',
-  styleUrls: ['./health.component.scss']
+  styleUrls: ['./health.component.scss'],
 })
 export class HealthComponent implements OnInit {
-
-  constructor() { }
+  url: string = '';
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    this.url = this.router.url;
   }
-
 }
