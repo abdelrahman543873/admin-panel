ae776293e9f441cad667ee3fea4c236e510777e7
abdelrahman mousa <abdelrahmanmysara33@gmail.com>
2022-09-16 18:20:44 +0200

    feat: added health check to express

--- frontend/server.js
+++ frontend/server.js
@@ -5,11 +5,14 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/frontend'));
+app.get('/frontend/health', function (req, res) {
+  return 'Ok';
+});
 app.get('/frontend/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(80, '0.0.0.0', () => {
-  console.log('app served')
+  console.log('app served');
 });
