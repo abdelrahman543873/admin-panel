d2e34f02cb7cef1cbfc052e01c41eaee81583c74
abdelrahman mousa <abdelrahmanmysara33@gmail.com>
2022-09-17 14:12:06 +0200

    feat: added cors policy to angular server

--- frontend/server.js
+++ frontend/server.js
@@ -2,8 +2,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const cors = require('cors');
 
 // Serve only the static files form the dist directory
+app.use(cors());
 app.use(express.static(__dirname + '/dist/frontend'));
 app.get('/frontend/health', function (req, res) {
   res.json('OK');
