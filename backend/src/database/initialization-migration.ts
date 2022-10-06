757cf3dc597c7797bba0d29232004e8bcbc5df12
abdelrahman <a.mysara@bonat.io>
2022-10-06 15:50:09 +0200

    hotfix: removed done migrations

--- backend/src/database/initialization-migration.ts
+++ backend/src/database/initialization-migration.ts
@@ -4,48 +4,48 @@ export class migration1664322329124 implements MigrationInterface {
   name = 'migration1664322329138';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE \`Merchant\` CHANGE \`link\` \`link\` varchar(255) NULL`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE \`Admin\` ADD \`name\` varchar(255) NOT NULL`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE \`Campaign\` ADD \`logo\` varchar(255) NOT NULL`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE \`Admin\` ADD \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE \`Admin\` ADD \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE \`Admin\` ADD \`refreshtoken\` varchar(255) NULL`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE \`Admin\` ADD \`refreshtokenexp\` date NULL`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE \`Admin\` CHANGE \`password\` \`password\` varchar(255) NOT NULL`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE \`Branch\` CHANGE \`posIdBranch_branch\` \`posIdBranch_branch\` varchar(255) NULL`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE \`Merchant\` ADD \`brandKey\` varchar(255) NULL`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE \`Merchant\` ADD \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE \`Merchant\` CHANGE \`link\` \`link\` varchar(255) NULL`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE \`Merchant\` CHANGE \`password\` \`password\` varchar(255) NOT NULL`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE \`Merchant\` DROP INDEX \`IDX_ce88f5788990d08fc957067b3f\``,
-    );
+    // await queryRunner.query(
+    //   `ALTER TABLE \`Merchant\` CHANGE \`link\` \`link\` varchar(255) NULL`,
+    // );
+    // await queryRunner.query(
+    //   `ALTER TABLE \`Admin\` ADD \`name\` varchar(255) NOT NULL`,
+    // );
+    // await queryRunner.query(
+    //   `ALTER TABLE \`Campaign\` ADD \`logo\` varchar(255) NOT NULL`,
+    // );
+    // await queryRunner.query(
+    //   `ALTER TABLE \`Admin\` ADD \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)`,
+    // );
+    // await queryRunner.query(
+    //   `ALTER TABLE \`Admin\` ADD \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)`,
+    // );
+    // await queryRunner.query(
+    //   `ALTER TABLE \`Admin\` ADD \`refreshtoken\` varchar(255) NULL`,
+    // );
+    // await queryRunner.query(
+    //   `ALTER TABLE \`Admin\` ADD \`refreshtokenexp\` date NULL`,
+    // );
+    // await queryRunner.query(
+    //   `ALTER TABLE \`Admin\` CHANGE \`password\` \`password\` varchar(255) NOT NULL`,
+    // );
+    // await queryRunner.query(
+    //   `ALTER TABLE \`Branch\` CHANGE \`posIdBranch_branch\` \`posIdBranch_branch\` varchar(255) NULL`,
+    // );
+    // await queryRunner.query(
+    //   `ALTER TABLE \`Merchant\` ADD \`brandKey\` varchar(255) NULL`,
+    // );
+    // await queryRunner.query(
+    //   `ALTER TABLE \`Merchant\` ADD \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)`,
+    // );
+    // await queryRunner.query(
+    //   `ALTER TABLE \`Merchant\` CHANGE \`link\` \`link\` varchar(255) NULL`,
+    // );
+    // await queryRunner.query(
+    //   `ALTER TABLE \`Merchant\` CHANGE \`password\` \`password\` varchar(255) NOT NULL`,
+    // );
+    // await queryRunner.query(
+    //   `ALTER TABLE \`Merchant\` DROP INDEX \`IDX_ce88f5788990d08fc957067b3f\``,
+    // );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {}
