3cac062c53d64c223efa97a21bfd4d8df82f8c63
abdelrahman <a.mysara@bonat.io>
2022-11-14 11:23:48 +0100

    feat: added bonat infra

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  host: 'http://loadbalancer-817109552.us-west-2.elb.amazonaws.com/backend',
+  host: 'http://loadbalancer-1191738629.us-west-2.elb.amazonaws.com/backend',
 };
